9743cd38918018dae2251642a73a504b646fdb55
jdornemann <johann.dornemann.c@gmail.com>
2018-10-04 23:43:15 -0300

    Ejercicios

--- 2 arrays/ejercicio3.rb
+++ 2 arrays/ejercicio3.rb
@@ -46,11 +46,49 @@ puts "La suma es:" + sumarray(a).to_s
 
 
 puts " # 3. Crear un método para obtener el promedio de un arreglo."
+puts "ALternativa 1"
 a = [1,2,3,9,1,4,5,2,3,6,6]
 
+def metodo1 (arreglo)
+    prom = arreglo.sum.to_f / arreglo.length
 
+    puts prom
+end
+puts "ALternativa 2"
 
+def metodo2(arreglo)
+    prom2 =arreglo.inject(0.0) { |sum, el| sum + el } / arreglo.size
 
+    puts prom2
+end
+
+metodo1(a)
+
+metodo2(a)
 
 puts " # 4. Crear un método que incrementa todos los elementos en una unidad y devuelva un arreglo nuevo."
+a = [1,2,3,9,1,4,5,2,3,6,6]
+
+def incrementa(arreglo)
+   b = Array.new
+    arreglo.each do |elem|
+        nuevo = elem + 1
+        b.push(nuevo)
+    end
+    i = 0
+    b.each do |elem|
+        puts "Original:" + arreglo[i].to_s + " Incrementado:" + elem.to_s
+        i += 1
+    end
+
+end
+
+
+ incrementa(a)
+
+
+
+
+
+
 
